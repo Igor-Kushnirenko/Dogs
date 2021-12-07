9e930876cd9367a2cdb4631970695459f499b94d
Igor-Kushnirenko <kushnirenko.ihor@lll.kpi.ua>
2021-12-07 22:33:20 +0200

    Example

---
+++ app/src/main/java/com/example/dogs_app/data/DataProvider.kt
@@ -0,0 +1,105 @@
+package com.example.dogs_app.data
+
+import com.example.dogs_app.R
+
+object DataProvider {
+
+    val puppyList = listOf(
+        Puppy(
+            id = 1,
+            title = "Хатіко",
+            sex = "Хлопчик",
+            age = 2,
+            description = "Хатіко любить чекати.",
+            puppyImageId = R.drawable.p1
+        ),
+        Puppy(
+            id = 2,
+            title = "Валенсія",
+            sex = "Дівчинка",
+            age = 6,
+            description = "Ваденсія любить вдумливі дискусії біля багаття .",
+            puppyImageId = R.drawable.p2
+        ),
+        Puppy(
+            id = 3,
+            title = "Зерон",
+            sex = "Хлопчик",
+            age = 5,
+            description = "Улюблене заняття Зерону - ганятись за своїм хвостом.",
+            puppyImageId = R.drawable.p3
+        ),
+        Puppy(
+            id = 4,
+            title = "Гіфт",
+            sex = "Хлопчик",
+            age = 1,
+            description = "Гіфт - справжній подарунок для тих, хто хоче слухача.",
+            puppyImageId = R.drawable.p4
+        ),
+        Puppy(
+            id = 5,
+            title = "Гледіс",
+            sex = "Дівчинка",
+            age = 10,
+            description = "Гледіс любить ловити м'яч, навіть якщо він для гри у регбі.",
+            puppyImageId = R.drawable.p5
+        ),
+        Puppy(
+            id = 6,
+            title = "Люсі",
+            sex = "Дівчинка",
+            age = 8,
+            description = "Будьте обережні, Люсі може власноруч зробити каст для одного відомого фільму.",
+            puppyImageId = R.drawable.p6
+        ),
+        Puppy(
+            id = 7,
+            title = "Бласто",
+            sex = "Хлопчик",
+            age = 1,
+            description = "Це птаха? Це літак? Ні, це Бласто вривається у ваше серце!",
+            puppyImageId = R.drawable.p7
+        ),
+        Puppy(
+            id = 8,
+            title = "Цербера",
+            sex = "Дівчинка",
+            age = 9,
+            description = "Будьте обережні, Цербера добре ховається зимою.",
+            puppyImageId = R.drawable.p8
+        ),
+        Puppy(
+            id = 9,
+            title = "Тайсен",
+            sex = "Хлопчик",
+            age = 7,
+            description = "Тайсен просто обожнює будь-які рухомі ігри.",
+            puppyImageId = R.drawable.p9
+        ),
+        Puppy(
+            id = 10,
+            title = "Зузу",
+            sex = "Дівчинка",
+            age = 5,
+            description = "Зузу майже ніколи не вилазить із своєї пересувної сумочки.",
+            puppyImageId = R.drawable.p10
+        ),
+        Puppy(
+            id = 11,
+            title = "Вальгала",
+            sex = "Дівчинка",
+            age = 2,
+            description = "Вальгала обожнює дивитись як тече вода, як горить вогонь, та як ти працюєш.",
+            puppyImageId = R.drawable.p11
+        ),
+        Puppy(
+            id = 12,
+            title = "Маркус",
+            sex = "Хлопчик",
+            age = 17,
+            description = "Маркусу вже нічого не потрібно, адже він спробував все.",
+            puppyImageId = R.drawable.p12
+        ),
+    )
+}
